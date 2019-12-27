d1843b1e82d600bb98db8a6537596311826c3880
Ashu <orangehat.rb@gmail.com>
2019-12-27 11:00:42 +0800

    Upload bird

---
+++ bird/build/precache-manifest.fa00c4626f8332e2516e046508d0d5f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3a46d6ef437245b2c2d122bfd235288",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc002f9d78d2ed93ca20",
+    "url": "/static/css/main.bb6b9b86.chunk.css"
+  },
+  {
+    "revision": "ab7d9f9d0b182ab45cc9",
+    "url": "/static/js/2.9019707f.chunk.js"
+  },
+  {
+    "revision": "b1bd7d111be11bf1a60de02093538313",
+    "url": "/static/js/2.9019707f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cc002f9d78d2ed93ca20",
+    "url": "/static/js/main.1e553c0a.chunk.js"
+  },
+  {
+    "revision": "1ea93026740272f1d446",
+    "url": "/static/js/runtime-main.ad1be643.js"
+  }
+]);
